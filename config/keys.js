cbbe0e56a070b63613ff4a00804fcb7d3c4f1b07
dustinkiselbach <52965183+dustinkiselbach@users.noreply.github.com>
2020-04-28 12:21:35 -0700

    gitingore fix

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://dustinkiselbach:Bass6969@musicianshome-jnofc.mongodb.net/test?retryWrites=true&w=majority',
-  secretOrKey: 'secret',
-  as3Key: 'AKIAI6QH75WUZVDLM53A'
-}
